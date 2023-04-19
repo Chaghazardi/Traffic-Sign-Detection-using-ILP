23055d081161cfa00dab9a50214e519ff8b03d9a
Chaghazardi <107412062+Chaghazardi@users.noreply.github.com>
2023-04-19 10:52:36 +0100

    Add files via upload

---
+++ ILP-based/Aleph/pos_Art.f
@@ -0,0 +1,22 @@
+
+ 
+traffic_sign(1, stopsign).
+traffic_sign(2, stopsign).
+traffic_sign(3, stopsign).
+traffic_sign(4, stopsign).
+traffic_sign(5, stopsign).
+traffic_sign(6, stopsign).
+traffic_sign(7, stopsign).
+traffic_sign(8, stopsign).
+
+
+ 
+traffic_sign(12, stopsign).
+traffic_sign(13, stopsign).
+traffic_sign(14, stopsign).
+traffic_sign(15, stopsign).
+traffic_sign(22, stopsign).
+traffic_sign(23, stopsign).
+traffic_sign(32, stopsign).
+traffic_sign(42, stopsign).
+
---
+++ ILP-based/Aleph/pos_GTSRB.f
@@ -0,0 +1,17 @@
+
+ 
+traffic_sign(pseudogerman-largebandwrectangles-basement_05ft_15deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_05ft_30deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_05ft_45deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_05ft_60deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_10ft_0deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_10ft_15deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_10ft_30deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_15ft_0deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_15ft_15deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_20ft_0deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_20ft_15deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_25ft_0deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_30ft_0deg, stopsign).
+traffic_sign(pseudogerman-largebandwrectangles-basement_40ft_0deg, stopsign).
+ 
